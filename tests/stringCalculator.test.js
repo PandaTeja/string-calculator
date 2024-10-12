f78e5866b738355344430e23dee32ba7d3a2a677
Eswara Teja <teja.panda@gmail.com>
2024-10-12 16:01:33 +0530

    changed code according to the three laws

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,30 +1,31 @@
 const { add } = require('../src/StringCalculator');
 
-test('returns 0 for an empty string', () => {
-  expect(add("")).toBe(0);
-});
+describe('String Calculator', () => {
+    it('should return 0 for an empty string', () => {
+        expect(add("")).toEqual(0);
+    });
 
-test('should return the number for a single number string', () => {
-    expect(add("1")).toEqual(1);
-});
+    it('should return the number when a single number is passed', () => {
+        expect(add("1")).toEqual(1);
+    });
 
-test('should return the sum of two numbers', () => {
-    expect(add("1,2")).toEqual(3);
-});
+    it('should return the sum of two numbers', () => {
+        expect(add("1,2")).toEqual(3);
+    });
 
-test('should return the sum of multiple numbers', () => {
-    expect(add("1,2,3")).toEqual(6);
-});
+    it('should return the sum of multiple numbers', () => {
+        expect(add("1,2,3")).toEqual(6);
+    });
 
-test('should handle new lines between numbers', () => {
-    expect(add("1\n2,3")).toEqual(6);
-});
+    it('should handle new lines between numbers', () => {
+            expect(add("1\n2,3")).toEqual(6);
+    });
 
-it('should support different delimiters', () => {
-    expect(add("//;\n1;2")).toEqual(3);
-});
+    it('should support custom delimiters', () => {
+        expect(add("//;\n1;2")).toEqual(3);
+    });
 
-it('should throw an exception for negative numbers', () => {
-    expect(() => add("1,-2,3,-4")).toThrowError('negative numbers not allowed: -2,-4');
+    it('should throw an exception for negative numbers', () => {
+        expect(() => add("1,-2,3,-4")).toThrowError('negative numbers not allowed: -2,-4');
+    });
 });
-
